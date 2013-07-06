d019066d453ff23f1d7e2984124a7b7871d9dbcf
Buddy Magsipoc <keikun17@gmail.com>
2013-07-06 10:09:02 +0800

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EsportsphDota::Application.config.secret_key_base = 'ff6f82544dcafc0bcdcdcb8677cac836a34a661fef72cf58de53d7df13b5effe6d3313cc5e96cafd338b33322efb75139f2798a6a562a6bf5e14127504a0b409'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EsportsphDota::Application.config.session_store :cookie_store, key: '_esportsph-dota_session'
