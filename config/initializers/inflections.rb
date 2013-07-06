296fa55214cdd006203b60b9a47bd66b90e028d2
Buddy Magsipoc <keikun17@gmail.com>
2013-07-06 12:12:03 +0800

    'hero'.pluralize => 'heroes'

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -10,6 +10,11 @@
 #   inflect.uncountable %w( fish sheep )
 # end
 
+# Non Locale Specific
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.plural(/^(hero)$/i, '\1es')
+end
+
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
