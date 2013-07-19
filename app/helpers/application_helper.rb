fc285dcef9c59f4f35ca2cd2ebb6f472d6e9bb9a
Buddy Magsipoc <keikun17@gmail.com>
2013-07-20 01:01:54 +0800

    New object takes defaults.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,13 @@ module ApplicationHelper
     f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(#{target})", class: 'btn btn-small btn-danger')
   end
 
-  def link_to_add_fields(name, f, association, target = 'this')
-    new_object = f.object.class.reflect_on_association(association).klass.new
+  # options :
+  #   attrs : attributes of the new associated object
+  def link_to_add_fields(name, f, association, attrs = {}, target = 'this'  )
+
+    new_object = f.object.class.reflect_on_association(association).klass.new(attrs)
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_fields", :f => builder)
+      render(association.to_s.singularize + "_fields", f: builder)
     end
     link_to_function(name, "add_fields(#{target}, \"#{association}\", \"#{escape_javascript(fields)}\")", class: 'btn btn-small btn-success')
   end
