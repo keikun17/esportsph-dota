00ab60bf09290982522c913f8de4ad7370a5d5d3
Buddy Magsipoc <keikun17@gmail.com>
2013-07-20 01:03:13 +0800

    Validate presence of team_id.

--- app/models/player_match.rb
+++ app/models/player_match.rb
@@ -7,6 +7,7 @@ class PlayerMatch < ActiveRecord::Base
 
   validates :player_id, presence: true
   validates :match_id, presence: true
+  validates :team_id, presence: true
 end
 
 # == Schema Information
