f935472abd39188c30228a9301c73f2b412001d3
Buddy Magsipoc <keikun17@gmail.com>
2013-08-08 01:25:15 +0800

    Team test.

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe Team do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'with 3 wins and 5 matches' do
+
+    let(:matches) { double(Object, count: 5) }
+    subject { described_class.new.stub({win_count: 3, matches: matches} ) }
+
+    it "has a loss count of 2" do
+      should have(2).loss_count
+    end
+  end
 end
 
 # == Schema Information
